9391728eb458d158dd987b3e8639cf0d28926bf6
erikn775 <erikn775@gmail.com>
2021-01-07 17:23:59 -0700

    starting building category model/controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,37 @@
 class CategoriesController < ApplicationController
+
+    def index
+        @categories = Category.all
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            redirect_to categories_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @category = Category.find_by(id: params[:id])
+    end
+
+    def update
+        @category = Category.find_by(id: params[:id])
+        @category.update(category_params)
+        redirect_to category_path(@category)
+    end
+
+    def destroy
+        @category = Category.find_by(id: params[:id])
+        @category.destroy
+        redirect_to categories_path
+    end
+
+    private
+    
+    def category_params
+        params.require(:category).permit(:name)
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,4 @@
 module ApplicationHelper
     
-        def logged_in?
-          !!session[:user_id]
-        end
-    
-        def current_user
-          User.find(session[:user_id])
-        end
       
 end
